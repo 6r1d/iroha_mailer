46b15119e5bba5953f2d0d8ab61a987f478b3ad3
6r1d <vic.6r1d@gmail.com>
2023-10-27 10:22:38 +0300

    Simplify the rendering code
    Signed-off-by: 6r1d <vic.6r1d@gmail.com>

--- server/render.py
+++ server/render.py
@@ -2,29 +2,42 @@
 Internal template code with the async support.
 """
 
-from jinja2 import Template
-from jinja2 import FileSystemLoader, Environment
-from filesystem import get_code_dir
 from tomllib import loads
+from jinja2 import FileSystemLoader, Environment
 
-def decode_template_data(input: str, mode: str):
-    data = loads(input)
+def decode_template_data(serialized: str, mode: str):
+    """
+    Deserialize and update the template data.
+
+    TODO:
+        refactor
+    """
+    data = loads(serialized)
     data['mode'] = mode
     return data
 
 class Renderer:
+    """
+    A common rendering utility class.
+    Used for both site and e-mail templates.
+
+    TODO:
+        introduce the template caching for the site templates
+    """
 
     def __init__(self, template_path:str, template_file:str='index.html'):
+        """
+        Configure the internal paths
+        """
         self.template_path = template_path
         self.template_file = template_file
 
-    async def prepare_template(self):
-        templateLoader = FileSystemLoader(searchpath=self.template_path)
-        templateEnv = Environment(loader=templateLoader, enable_async=True)
-        template = templateEnv.get_template(self.template_file)
-        return template
-
-    async def render_template(self, template_data):
-        page_template = await self.prepare_template()
+    async def render_template(self, template_data: dict):
+        """
+        Load and render a template using the provided data.
+        """
+        template_loader = FileSystemLoader(searchpath=self.template_path)
+        template_env = Environment(loader=template_loader, enable_async=True)
+        page_template = template_env.get_template(self.template_file)
         rendered_template = await page_template.render_async(template_data)
         return rendered_template
