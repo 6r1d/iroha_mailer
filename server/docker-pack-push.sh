6b0c483981631f190e340955d313b7a3266c1fb5
safinsaf <safinsaft@gmail.com>
2023-11-20 20:45:05 +0300

    Add docker publish scripts
    Signed-off-by: safinsaf <safinsaft@gmail.com>

---
+++ server/docker-pack-push.sh
@@ -0,0 +1,12 @@
+if [ "$REF" = "refs/heads/main" ]; then
+  TAG=latest
+elif [[ "$REF"  == refs/tags/* ]]; then
+  TAG="${REF#refs/tags/}"
+else
+  TAG=dev
+fi
+
+FULL_TAG="soramitsu/iroha2-mailer-server:${TAG}"
+
+docker build -t $FULL_TAG .
+docker push $FULL_TAG
